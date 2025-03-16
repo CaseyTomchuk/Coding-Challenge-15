3402f863c2284d41b8781ede294a5e4219df2916
Casey Tomchuk <Caseytomchuk@gmail.com>
2025-03-16 17:17:41 -0400

    Task 4 - Risk Categorization

--- cc_15.js
+++ cc_15.js
@@ -33,10 +33,36 @@ newCard.appendChild(resolveButton);
 resolveButton.addEventListener("click", function() {
     riskSelector.removeChild(newCard);
 });
-    riskSelector.appendChild(newCard); // appending newCard with all its changes to riskSelector
+
+// Task 4: Categorizing Risks by Level
+
+// Applying some basic styling to make it look more appealing (idk if i lose points for unnecessary code)
+newCard.style.padding = `10px`;
+newCard.style.margin = '10px';
+newCard.style.borderRadius = '5px';
+newCard.style.display ='inline-block'; // Taken from previous coding challenges 
+
+// Applying color to the card based on riskLevel
+
+if (riskLevel === "Low") {
+    newCard.style.backgroundColor = 'lightgreen';
+}
+else if (riskLevel === "Medium") {
+    newCard.style.backgroundColor = 'yellow';
+}
+else if (riskLevel === "High") {
+    newCard.style.backgroundColor = 'red';
+}
+else {
+    newCard.style.backgroundColor = 'gray';
+}
+
+// appending newCard with all its changes to riskSelector
+    riskSelector.appendChild(newCard);
 };
 
-addRiskItem("Market Fluctuations", "High", "Finance");
+addRiskItem("Cybersecurity Threat", "High", "IT");
+addRiskItem("HR Compliance Issue", "Low", "Human Resources");
 
 const submitButton = document.getElementById("buttonId"); // links the submit button to javascript
 
